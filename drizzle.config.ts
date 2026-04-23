9df070f2819d5d69d0a0a4671cb59aa77d6cb740
arpan-jain <ajain@offchainlabs.com>
2026-04-23 16:08:03 +0530

    fixed lint failed check

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     out: "./src/db/migrations",
     dialect: "postgresql",
     dbCredentials: {
-        url: process.env.DATABASE_URL!
+        url: process.env.DATABASE_URL ?? ""
     }
 })
